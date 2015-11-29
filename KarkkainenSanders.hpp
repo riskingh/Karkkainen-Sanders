cbe80887da1b1520ae4dcb43c4084843ee0ed96a
Maxim Grishkin <grishkin.max@gmail.com>
2015-11-29 12:09:43 +0300

    initial commit

---
+++ KarkkainenSanders.hpp
@@ -0,0 +1,14 @@
+//
+//  KarkkainenSanders.hpp
+//  Karkkainen-Sanders
+//
+//  Created by Maxim Grishkin on 29/11/15.
+//  Copyright © 2015 Maxim Grishkin. All rights reserved.
+//
+
+#ifndef KarkkainenSanders_hpp
+#define KarkkainenSanders_hpp
+
+#include <stdio.h>
+
+#endif /* KarkkainenSanders_hpp */
