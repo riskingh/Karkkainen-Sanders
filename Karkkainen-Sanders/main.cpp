b686fcb2008b0e8c1f59d4d8544589effff26647
Maxim Grishkin <grishkin.max@gmail.com>
2015-11-29 12:07:23 +0300

    Initial Commit

---
+++ Karkkainen-Sanders/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Karkkainen-Sanders
+//
+//  Created by Maxim Grishkin on 29/11/15.
+//  Copyright © 2015 Maxim Grishkin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
